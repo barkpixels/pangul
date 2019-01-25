02b836ef96b724306ceb57104d25ddc51fe23fdb
Sajjad Heydari <mcshemail@gmail.com>
2019-01-25 11:41:09 +0330

    Resolved #2

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
+
+const request = require('request');
+
 const CONF_FILE = "./conf.json";
 
 fs.readFile(CONF_FILE, (err, data)=>{
   if(err)
     console.error(data);
   let conf = JSON.parse(data.toString());
-  console.log(conf)
+  console.log("Finished parsing config");
+  run_check(conf);
 });
+
+function run_check(conf){
+  conf.sites.map(health_check);
+}
+
+function health_check(url){
+  request(url, (err, resp, body)=>{
+    if(err){
+      console.error(url + ": fail");
+      return;
+    }
+    console.log(url + ": ok");
+  });
+}
